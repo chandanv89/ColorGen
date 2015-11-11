bf294ce87d2dc63dcb5e3d0d57127ceb2f9a077c
Chandan Veerabhadrappa <chandanv89@gmail.com>
2015-11-11 11:49:18 +0530

    Modified font and script behavior
    * Changed the font from 'Archivo Narrow' to 'Segnika Negative'
    * Sorted the CSS properties
    * Updated the script to use random colors on page load
    * Minified some of the (borrowed) JS

--- js/script.js
+++ js/script.js
@@ -1,44 +1,9 @@
 
-/* colourBrightness.js by @jamiebrittain */
-(function(e){
-   e.fn.colourBrightness=function(){
-      var e,t,n,r,i=this.css("background-color");
-      if(i.match(/^rgb/)){
-         i=i.match(/rgb\(([^)]+)\)/)[1];
-         i=i.split(/ *, */).map(Number);
-         e=i[0];
-         t=i[1];
-         n=i[2]
-      }else if("#"==i[0]&&7==i.length){
-         e=parseInt(i.slice(1,3),16);
-         t=parseInt(i.slice(3,5),16);
-         n=parseInt(i.slice(5,7),16)
-      }else if("#"==i[0]&&4==i.length){
-         e=parseInt(i[1]+i[1],16);
-         t=parseInt(i[2]+i[2],16);
-         n=parseInt(i[3]+i[3],16)
-      }
-      r=(e*299+t*587+n*114)/1e3;
-      r<125 ? this.removeClass("light").addClass("dark") : this.removeClass("dark").addClass("light")
-   }
-})(jQuery);
+/* colourBrightness.js by @jamiebrittain | minified on http://javascript-minifier.com */
+!function(s){s.fn.colourBrightness=function(){var s,a,e,r,t=this.css("background-color");t.match(/^rgb/)?(t=t.match(/rgb\(([^)]+)\)/)[1],t=t.split(/ *, */).map(Number),s=t[0],a=t[1],e=t[2]):"#"==t[0]&&7==t.length?(s=parseInt(t.slice(1,3),16),a=parseInt(t.slice(3,5),16),e=parseInt(t.slice(5,7),16)):"#"==t[0]&&4==t.length&&(s=parseInt(t[1]+t[1],16),a=parseInt(t[2]+t[2],16),e=parseInt(t[3]+t[3],16)),r=(299*s+587*a+114*e)/1e3,125>r?this.removeClass("light").addClass("dark"):this.removeClass("dark").addClass("light")}}(jQuery);
 
-/* selectText.js by @jamiebrittain */
-!function(e){
-   e.fn.selectText=function(){
-      var e=this[0];
-      if(document.body.createTextRange){
-         var t=document.body.createTextRange();
-         t.moveToElementText(e),t.select()
-      }else if(window.getSelection){
-         var n=window.getSelection(),
-         t=document.createRange();
-         t.selectNodeContents(e),
-         n.removeAllRanges(),
-         n.addRange(t)
-      }
-   }
-}(jQuery);
+/* selectText.js by @jamiebrittain | minified on http://jscompress.com */
+!function(e){e.fn.selectText=function(){var e=this[0];if(document.body.createTextRange){var t=document.body.createTextRange();t.moveToElementText(e),t.select()}else if(window.getSelection){var n=window.getSelection(),t=document.createRange();t.selectNodeContents(e),n.removeAllRanges(),n.addRange(t)}}}(jQuery);
 
 var generateColor = function(){
    var hexChars = "0123456789ABCDEF";
@@ -65,13 +30,19 @@ var updateColor = function(color){
    updateUrl(color);
 }
 
-window.onload = function(){
+var init = function(){
    document.body.style.overflow = "hidden";
    $("body").css('height', $(window).height()).css('width', $(window).width());
+   $("#canvas").css('backgroundColor',generateColor()).colourBrightness();
+}
+
+window.onload = function(){
+   var color = "";
+   init();
    
    $("body").on('keypress', function(e){
-      if("32" == e.keyCode){
-         var color = generateColor();
+      if(e.keyCode){
+         color = generateColor();
          updateColor(color);
       }
    });
